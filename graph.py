305a5cecbba689e556e7c94b2509be85cd0b56c5
jakebillings <jake@jakebillings.com>
2017-05-17 21:05:11 -0600

    Initial commit

---
+++ graph.py
@@ -0,0 +1,60 @@
+import numpy
+import networkx as nx
+import gui
+
+# Count the triangles in an adjacency matrix
+def count_triangles(matrix):
+    print "matrix"
+    print matrix
+    squared = numpy.dot(matrix, matrix)
+    cubed = numpy.dot(squared, matrix)
+    print "cubed"
+    print cubed
+    diagonal = numpy.diagonal(cubed)
+    print "diagonal"
+    print diagonal
+    sum = numpy.sum(diagonal)
+    print "sum", sum
+    return sum/6
+
+# Recursively generate edges for a tree graph
+# width describes the number of branches to create at each node. width is n where the desired tree is an n tree
+# depth the number of recursions to perform. Depth levels of tree will be generated from the first node.
+def tree(width, depth, root=1, nodeIndex=1):
+    layer = []
+    if depth is 0:
+        return []
+    for i in range(root, root+width):
+        nodeIndex+=1
+        node = nodeIndex
+
+        layer.append((root, node, 1))
+        subLayer=tree(width, depth-1,root=node,nodeIndex=node)
+        layer += subLayer
+        nodeIndex += len(subLayer)
+
+    return layer
+
+# Wrap the result of tree() in a networkx Graph
+def treex(width, depth, nodeIndex=1):
+    G=nx.Graph()
+    G.add_weighted_edges_from(tree(width,depth,nodeIndex))
+    return G
+
+if __name__=="__main__":
+    # Generate a tree of width 3 with a depth of 1
+    G = treex(3,1)
+
+    # Count the triangles and print the edges (there should be 0 triangles)
+    print count_triangles(nx.to_numpy_matrix(G)), "triangles in a basic tree graph:"
+    print G.edges()
+
+    # Add an edge connecting two branches. This creates a triangle.
+    G.add_edge(2,3)
+
+    # Count the triangles and print the edges of the new tree (there should be 1 triangle)
+    print count_triangles(nx.to_numpy_matrix(G)), "triangles in a tree graph where two branches have been connected:"
+    print G.edges()
+
+    # Draw the graph containing the tree with two connected branches
+    gui.draw_network(G)
---
+++ gui.py
@@ -0,0 +1,54 @@
+from Tkinter import *
+import networkx as nx
+
+WIDTH = 1024
+HEIGHT = 480
+ROW_HEIGHT = 64
+CENTER = WIDTH / 2
+NODE_WIDTH = 32
+
+
+# Quick and dirty Tkinter function to visualize tree-like graphs from networkx
+# Layers are determined using the shortest path to the root node
+def draw_network(G, root=1):
+    master = Tk()
+
+    w = Canvas(master, width=WIDTH, height=480)
+    w.pack()
+    layer_nodes = {}
+
+    # Calculate node locations
+    nodeCoordinates = {}
+
+    for node in G.nodes():
+        # The layer of each node is its shortest distance from the root node
+        layer = len(nx.shortest_path(G, node, root))
+
+        if not layer in layer_nodes.keys():
+            layer_nodes[layer] = []
+        layer_nodes[layer].append(node)
+
+    for layer in layer_nodes.keys():
+        nodes = layer_nodes[layer]
+        index = 0
+        for node in sorted(nodes):
+            x1 = CENTER - NODE_WIDTH * (len(nodes) / 2 - index)
+            y1 = ROW_HEIGHT * layer
+
+            nodeCoordinates[node] = (x1, y1)
+
+            index += 1
+
+    # Draw edges
+    for edge in G.edges():
+        a = nodeCoordinates[edge[0]]
+        b = nodeCoordinates[edge[1]]
+        w.create_line(a[0], a[1], b[0], b[1])
+
+    # Draw nodes
+    for node in G.nodes():
+        coordinates = nodeCoordinates[node]
+        w.create_oval(coordinates[0] - 12, coordinates[1] - 12, coordinates[0] + 12, coordinates[1] + 12, fill="white")
+        w.create_text(coordinates[0], coordinates[1], text=str(node))
+
+    mainloop()
