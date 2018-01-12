314894ef3f485b75a733464cd31d87ca85dedbff
jakebillings <jake@jakebillings.com>
2018-01-11 22:17:04 -0700

    Implement upper_bound from 2.1.2 of the huntington dissertation

--- find_extremal_graphs.py
+++ find_extremal_graphs.py
@@ -1,7 +1,22 @@
 # This is a work-in-progress psuedocode file for the final extremal graph finding algorithm
+from math import pow, sqrt, floor
+
+
+# upper_bound()
+#
+# equivalent to f(n) from the dissertation (2.3) pg. 31
+# see 2.1.2 on pg 25 of the dissertation
+#
+# equivalent
+def upper_bound(n):
+    return floor(((n / 12.0) * (pow(((108.0 * n) - 188.0 + (12.0 * sqrt(3.0)) * sqrt(83.0 - (94.0 * n) + (27.0 * n * n))), 1.0/3.0))) \
+     - ((n / 12.0) * (pow(((-108.0 * n) + 188.0 + (12.0 * sqrt(3.0)) * sqrt(83.0 - (94.0 * n) + (27.0 * n * n))), 1.0/3.0))) \
+     + (n / 3.0))
 
 
 # find_extremal_graph
+#
+# equivalent to s(n) from the dissertation (2.3) pg. 31
 #
 # finds an extremal graph for n vertices that follows the restrictions of this research:
 #
@@ -16,3 +31,28 @@ def find_extremal_graph(n):
                     # 1. E >= 1/2q(q+1)^2 (https://faculty.math.illinois.edu/~z-furedi/PUBS/furedi_C4from1988.pdf)
                     # 2. Bound from Huntington's research
     pass
+
+
+# test():
+#
+# test our functions using known values from the dissertation (2.3) pg. 31
+def test():
+    print 'Testing upper_bound()'
+    assert upper_bound(6) == 6
+    assert upper_bound(7) == 7
+    assert upper_bound(8) == 9
+    assert upper_bound(9) == 11
+    assert upper_bound(10) == 12
+    assert upper_bound(11) == 14
+    assert upper_bound(12) == 16
+    assert upper_bound(13) == 18
+    assert upper_bound(14) == 20
+    assert upper_bound(14) == 20
+    assert upper_bound(15) == 22
+    assert upper_bound(16) == 24
+    print 'Passed.'
+
+
+# if anybody bothers to run this, run the test() function
+if __name__ == '__main__':
+    test()
---
+++ notes/notes.md
@@ -0,0 +1,11 @@
+From dissertation pg 32:
+
+A much more accessible question is to search for extremal graphs for ex(40, {C3, C4, C6}). Even though a polarity graph
+of a generalized quadrangle of order 3 does not exist, it still may be informative to find the extremal graphs on 40
+vertices. If a polarity graph did exist it would have 75 edges. So an open question is whether or not there are graphs
+on 40 vertices, with no 3, 4 or 6-cycles that have 75 edges or more.
+
+Links:
+Problems in extremal graph theory https://pdfs.semanticscholar.org/2883/5864fdd880047ca5b0bdc03762c8552fc0d5.pdf
+Furedi's work E >= 1/2q(q+1)^2 (https://faculty.math.illinois.edu/~z-furedi/PUBS/furedi_C4from1988.pdf)
+Huntington's Dissertation
